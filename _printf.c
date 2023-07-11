ac17ae2f8735d19067d2e3be01952817693d4816
Nick <nicholasyoung548@yahoo.com>
2023-07-11 09:01:34 -0700

    updated _printf.c

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	for (int i=0; format [i] != '\0'; i++)
+	{
+		if (format[i] == '%');
+		i++
+		
